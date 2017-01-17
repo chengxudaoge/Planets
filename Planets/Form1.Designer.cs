5ebf568acf050de835887079e4ed5436ea49c80b
doug rosener <chengxudaoge@gmail.com>
2017-01-16 21:15:11 -0800

    circling planet

--- Planets/Form1.Designer.cs
+++ Planets/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(86, 208);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button start;
     }
 }
 
--- Planets/Form1.cs
+++ Planets/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Planets
@@ -15,6 +16,37 @@ namespace Planets
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            double theta = 0;
+            int x=0, y=0;
+            while (true)
+            {
+                DrawIt(x, y, Color.White);
+                x = (int)(100*Math.Cos(theta)+90);
+                y = (int)(100*Math.Sin(theta)+90);
+                DrawIt(x, y,Color.Green);
+                theta = theta + 10*Math.PI / 180.0;
+                Thread.Sleep(100);
+            }
+        }
+
+        private void DrawIt(int x, int y, Color color)
+        {
+            System.Drawing.Graphics graphics = this.CreateGraphics();
+            //System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(
+            //    50, 100, 150, 150);
+            //graphics.DrawEllipse(System.Drawing.Pens.Black, rectangle);
+            //graphics.DrawRectangle(System.Drawing.Pens.Red, rectangle);
+            System.Drawing.SolidBrush brush1 = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+            graphics.FillEllipse(brush1, new System.Drawing.Rectangle(50, 50, 100, 100));
+            System.Drawing.SolidBrush brush2 = new System.Drawing.SolidBrush(color);
+            graphics.FillEllipse(brush2, new System.Drawing.Rectangle(x, y, 20, 20));
+            brush1.Dispose();
+            brush2.Dispose();
+            graphics.Dispose();
         }
     }
 }
